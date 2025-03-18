75d0e1a42912c8268c94de8d6b405ce89b14eaa3
eduardohinojosa1 <eduardohinojosa@fico.com>
2025-03-18 11:09:51 -0700

    Removed for loops from Professional Services

--- professional_services.vb
+++ professional_services.vb
@@ -1,12 +1,11 @@
 Option Explicit
 
-Const PS_MANAGING_QUESTION As String = "D10"
-Const RANGE_PS_MANAGING As String = "D11"
+Const CUST_APP_MGMT_QUESTION As String = "D10"
+Const CUST_APP_MGMT_HOW As String = "D11"
 
 Const SELF_SUPPORT_QUESTION As String = "D12"
-Const RANGE_SS_QUESTIONS_RANGE As String = "D13:D14"
-Const YES_SS_CASE_MAP As String = "D13"
 
+Const YES_SS_CASE_MAP As String = "D13"
 Const NO_SS_CASE_MAP As String = "D14"
 
 Const PREFERRED_VENDOR_CASE As String = "D15"
@@ -14,10 +13,10 @@ Const PREFERRED_VENDOR_CASE As String = "D15"
 Private Sub Worksheet_Activate()
     On Error Resume Next
 
-    ShowHideModelsQuestions Range(PS_MANAGING_QUESTION)
+    ShowHideManagingQuestion Range(CUST_APP_MGMT_QUESTION)
     ShowHideSelfSupportQuestions Range(SELF_SUPPORT_QUESTION)
     ShowHidePreferredVendorQuestion Range(NO_SS_CASE_MAP)
-    
+
     Dim Cell As Range
     For Each Cell In Application.ActiveSheet.UsedRange
         ThisWorkbook.CheckRequiredCell Cell
@@ -35,47 +34,42 @@ End Sub
 Sub Worksheet_Change(ByVal Target As Range)
     On Error Resume Next
     ThisWorkbook.UnProtect_This_Sheet
-    
+
     Dim Cell As Range
     For Each Cell In Application.ActiveSheet.UsedRange
         ThisWorkbook.CheckRequiredCell Cell
     Next
-    
+
     Target.AutoFit
-    
-    ShowHideModelsQuestions Range(PS_MANAGING_QUESTION)
+
+    ShowHideManagingQuestion Range(CUST_APP_MGMT_QUESTION)
     ShowHideSelfSupportQuestions Range(SELF_SUPPORT_QUESTION)
     ShowHidePreferredVendorQuestion Range(NO_SS_CASE_MAP)
-    
+
     ThisWorkbook.Protect_This_Sheet
 End Sub
-Sub ShowHideModelsQuestions(Target As Range)
+Sub ShowHideManagingQuestion(Target As Range)
     On Error Resume Next
     ThisWorkbook.UnProtect_This_Sheet
 
-    Dim modelsAnswer As Range
+    Dim appManagingAnswer As Range
 
     'did the change happen where we are interested?
-    Set modelsAnswer = Intersect(Target, Range(PS_MANAGING_QUESTION))
+    Set appManagingAnswer = Intersect(Target, Range(CUST_APP_MGMT_QUESTION))
 
   ' if the change happens outside of the range then ignore it
-    If modelsAnswer Is Nothing Then
+    If appManagingAnswer Is Nothing Then
         Exit Sub
     End If
-    
-    Dim r As Range
-    If modelsAnswer.Value2 <> "Mix of these" Or modelsAnswer.Value2 = "" Then
+
+    If appManagingAnswer.Value2 = "Internal Resources" Or appManagingAnswer.Value2 = "Third-party Consultants" Or appManagingAnswer.Value2 = "" Then
     ' hide the rows
-         For Each r In Range(RANGE_PS_MANAGING).Rows
-             r.EntireRow.Hidden = True
-         Next r
-         'Worksheets("Analytics").Visible = False
-    
-    ElseIf modelsAnswer.Value2 = "Mix of these" Then
+        Range(CUST_APP_MGMT_HOW).EntireRow.Hidden = True
+    End If
+
+    If appManagingAnswer.Value2 = "Mix of these" Then
         ' show the rows
-        For Each r In Range(RANGE_PS_MANAGING).Rows
-             r.EntireRow.Hidden = False
-         Next r
+        Range(CUST_APP_MGMT_HOW).EntireRow.Hidden = False
     End If
 
     ThisWorkbook.Protect_This_Sheet
@@ -85,42 +79,33 @@ Sub ShowHideSelfSupportQuestions(Target As Range)
     On Error Resume Next
     ThisWorkbook.UnProtect_This_Sheet
 
-    Dim modelsAnswer As Range
+    Dim customerSelfSupport As Range
 
     'did the change happen where we are interested?
-    Set modelsAnswer = Intersect(Target, Range(SELF_SUPPORT_QUESTION))
+    Set customerSelfSupport = Intersect(Target, Range(SELF_SUPPORT_QUESTION))
 
   ' if the change happens outside of the range then ignore it
-    If modelsAnswer Is Nothing Then
+    If customerSelfSupport Is Nothing Then
         Exit Sub
     End If
-
-    If modelsAnswer.Value2 = "" Then
-    ' hide the rows
-         Dim r As Range
-         For Each r In Range(RANGE_SS_QUESTIONS_RANGE).Rows
-             r.EntireRow.Hidden = True
-         Next r
     
-    ElseIf modelsAnswer.Value2 = "Yes" Then
+    If customerSelfSupport.Value2 = "" Or customerSelfSupport.Value2 = "Uncertain" Then
+    ' hide the rows
+        Range(YES_SS_CASE_MAP).EntireRow.Hidden = True
+        Range(NO_SS_CASE_MAP).EntireRow.Hidden = True
+    End If
+    
+    Dim r As Range
+    If customerSelfSupport.Value2 = "Yes" Then
         ' show the rows
-        For Each r In Range(YES_SS_CASE_MAP).Rows
-             r.EntireRow.Hidden = False
-         Next r
-         
-         For Each r In Range(NO_SS_CASE_MAP).Rows
-            r.EntireRow.Hidden = True
-         Next r
-         
-    ElseIf modelsAnswer.Value2 = "No" Then
-        For Each r In Range(NO_SS_CASE_MAP).Rows
-             r.EntireRow.Hidden = False
-         Next r
-         
-         For Each r In Range(YES_SS_CASE_MAP).Rows
-            r.EntireRow.Hidden = True
-         Next r
-         
+         Range(YES_SS_CASE_MAP).EntireRow.Hidden = False
+         Range(NO_SS_CASE_MAP).EntireRow.Hidden = True
+
+    End If
+
+    If customerSelfSupport.Value2 = "No" Then
+        Range(YES_SS_CASE_MAP).EntireRow.Hidden = True
+        Range(NO_SS_CASE_MAP).EntireRow.Hidden = False
     End If
 
     ThisWorkbook.Protect_This_Sheet
@@ -130,29 +115,23 @@ Sub ShowHidePreferredVendorQuestion(Target As Range)
     On Error Resume Next
     ThisWorkbook.UnProtect_This_Sheet
 
-    Dim modelsAnswer As Range
+    Dim preferredVendorAnswer As Range
 
     'did the change happen where we are interested?
-    Set modelsAnswer = Intersect(Target, Range(NO_SS_CASE_MAP))
+    Set preferredVendorAnswer = Intersect(Target, Range(NO_SS_CASE_MAP))
 
   ' if the change happens outside of the range then ignore it
-    If modelsAnswer Is Nothing Then
+    If preferredVendorAnswer Is Nothing Then
         Exit Sub
     End If
 
-    If modelsAnswer.Value2 = "Internal Resources" Or modelsAnswer.Value2 = "" Then
+    If preferredVendorAnswer.Value2 = "Internal Resources" Or preferredVendorAnswer.Value2 = "" Then
     ' hide the rows
-         Dim r As Range
-         For Each r In Range(PREFERRED_VENDOR_CASE).Rows
-             r.EntireRow.Hidden = True
-         Next r
-    
+        Range(PREFERRED_VENDOR_CASE).EntireRow.Hidden = True
     Else
         ' show the rows
-        For Each r In Range(PREFERRED_VENDOR_CASE).Rows
-             r.EntireRow.Hidden = False
-         Next r
+        Range(PREFERRED_VENDOR_CASE).EntireRow.Hidden = False
     End If
-
+    
     ThisWorkbook.Protect_This_Sheet
 End Sub
